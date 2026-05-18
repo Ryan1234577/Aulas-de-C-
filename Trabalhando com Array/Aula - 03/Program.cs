2688aa5d1739536119499c792d641e9dc9d30945
Ryan Marcos da Silva Costa <ryanmarcosdasilvacosta@gmail.com>
2026-05-18 17:10:05 -0300

    Aula de C#, 18/05/2026, ministrada pelo professor Wilton. Hoje realizamos o aprendizado com List, com um exemplo prático montando a lista da seleção brasileira e alguns comandos adicionais de edição de uma lista

--- Trabalhando com Array/Aula - 03/Program.cs
+++ Trabalhando com Array/Aula - 03/Program.cs
@@ -86,10 +86,12 @@ namespace Aula___03
 
 
             //matriz
-            int[,] matriz = { { 1, 2, 3, 4, 5, 6, 7, 8, 9, } };
-
-
-
+            //int[,] matriz = { 
+                //{ 1, 2, 3 }, 
+                //{ 4, 5, 6 }, 
+              //  {7, 8, 9, }
+            //};
+            //Console.WriteLine(matriz[1, 1]);
         }
     }
 }
---
+++ Trabalhando com List/List/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> Convocacao = new List<string>();
+
+            // Goleiros
+            Convocacao.Add("Hugo Souza");
+            Convocacao.Add("Ederson");
+            Convocacao.Add("Bento");
+
+            // Defensores
+            Convocacao.Add("Marquinhos");
+            Convocacao.Add("Danilo");
+            Convocacao.Add("Ibañez");
+            Convocacao.Add("Alex Sandro");
+            Convocacao.Add("Léo Pereira");
+            Convocacao.Add("Carlos Augusto");
+            Convocacao.Add("Wesley");
+
+            // Meio-Campistas
+            Convocacao.Add("Casemiro");
+            Convocacao.Add("Bruno Guimarães");
+            Convocacao.Add("Fabinho");
+            Convocacao.Add("Lucas Paquetá");
+            Convocacao.Add("Danilo");
+            Convocacao.Add("Andrey");
+
+            //Atacantes
+            Convocacao.Add("Vinicius Junior");
+            Convocacao.Add("Raphinha");
+            Convocacao.Add("Neymar");
+            Convocacao.Add("Endrick");
+            Convocacao.Add("Luiz Henrique");
+            Convocacao.Add("Matinelli");
+
+            foreach (string nome in Convocacao)
+            {
+                Console.WriteLine(nome);
+            }
+
+
+
+
+
+        }
+    }
+}
